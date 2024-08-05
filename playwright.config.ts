cb6547fa4565b1392216841cb4184bd217ba3087
oscarprdev <vlcoscar8@gmail.com>
2024-08-05 20:22:14 +0200

    Add dotenv on playwirght file config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
 
+require('dotenv').config();
+
 export default defineConfig({
 	testDir: './src/tests/e2e',
 	/* Run tests in files in parallel */
