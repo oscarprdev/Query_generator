868befb6859c34cbcb042d6dfd30de0b9e30eaa7
oscarprdev <vlcoscar8@gmail.com>
2024-08-05 20:53:38 +0200

    Update config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
 	webServer: {
 		command: 'npm run dev',
 		url: 'http://localhost:3000',
-		timeout: 5000,
+		timeout: 120 * 1000 * 5,
 		reuseExistingServer: false,
 	},
 
